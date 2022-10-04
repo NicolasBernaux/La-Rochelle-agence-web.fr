e6c57d09ecee66115763128b206dbeb0249ccef0
nicolasBernaux <bernaux.n@gmail.com>
2022-10-04 16:36:29 +0200

    Heroku test

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 
 app.listen(port, () => {
