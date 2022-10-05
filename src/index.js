b6c8713da32fcef7c55f019a2d5ac5458c579a1a
nicolasBernaux <bernaux.n@gmail.com>
2022-10-05 16:18:19 +0200

    Fix build errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
+import * as OfflinePluginRuntime from '@lcdp/offline-plugin/runtime';
 
 import './index.scss';
 import './scripts/script';
