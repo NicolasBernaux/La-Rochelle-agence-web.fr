bddb106d634d5c68e249bbbc0b072ee2be66b9a0
nicolasBernaux <bernaux.n@gmail.com>
2022-10-05 17:12:30 +0200

    Fix webpack

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('dist'));
+//app.use(express.static('dist'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
